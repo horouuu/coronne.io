13d5ae87210f3c0dbb4ad830858a0f05e6f3ba0e
horouuu <horou@coronne.io>
2025-11-03 01:45:19 +0800

    fix: use sessionStorage instead to store pagination states

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const sections = document.querySelectorAll("section");
 const backBtn = document.getElementById("backButton");
 const nextBtn = document.getElementById("nextButton");
 
+function getCurrentPage() {
+  return isNaN(parseInt(sessionStorage.getItem("currentPage")))
+    ? 0
+    : parseInt(sessionStorage.getItem("currentPage"));
+}
+
 const fixWindowScroll = () => {
+  const currentPage = getCurrentPage();
   const width = sections[currentPage].getBoundingClientRect().width;
   window.scrollTo(width * currentPage, 0);
 };
@@ -18,29 +25,26 @@ window.addEventListener("DOMContentLoaded", () => {
   const isReload = nav.type === "reload";
 
   if (!isReload) {
-    currentPage = 0;
-    localStorage.removeItem("currentPage");
+    sessionStorage.removeItem("currentPage");
   }
 
   updateButtons();
 });
 
 function updateButtons() {
+  const currentPage = getCurrentPage();
   backBtn.classList.toggle("edge", currentPage === 0);
   nextBtn.classList.toggle("edge", currentPage === sections.length - 1);
 }
 
-let currentPage = isNaN(parseInt(localStorage.getItem("currentPage")))
-  ? 0
-  : parseInt(localStorage.getItem("currentPage"));
-
 function getPage(diff) {
+  let currentPage = getCurrentPage();
   currentPage =
     diff > 0
       ? Math.min(currentPage + diff, sections.length - 1)
       : Math.max(currentPage + diff, 0);
 
-  localStorage.setItem("currentPage", currentPage);
+  sessionStorage.setItem("currentPage", currentPage);
   sections[currentPage].scrollIntoView({ behavior: "smooth" });
   updateButtons();
 }
